935619dfa611740bd6a6734713edfcf998f2d83d
Otávio Alves <oalves@protonmail.com>
2025-04-14 20:48:43 -0400

    Styling of talk list

--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -1,36 +1,29 @@
 
 import React from 'react';
 import talks from '../talks/talks.jsx'
+import '../styles/Catalog.scss'
 
 export default function Catalog(){
   return (
-    <div style={styles.catalog}>
+    <div className='talk-catalog'>
       <h1>My Talks</h1>
       {talks.map((talk) => (
-        <div key={talk.id} style={styles.talkCard}>
-          <h2><a href={`/${talk.id}`}>{talk.title}</a></h2>
-          <h3>{talk.subtitle}</h3>
-          <p>{talk.description}</p>
-          <p>{new Date(talk.date).toLocaleDateString()}</p>
+        
+ 
+ <a key={talk.id} href={`/${talk.id}`} className='talk-link'>
+          <div key={talk.id} className='talk-card'>
+          <div className="header" style={{
+            backgroundImage: `url(${talk.background})`,
+          }}></div>
+          <div className="content">
+            <h2>{talk.title}</h2>
+            <h3>{talk.subtitle}</h3>
+            <p>{talk.description}</p>
+            <p>{new Date(talk.date).toLocaleDateString('en-US', { year: 'numeric', month: 'long' })}</p>
+          </div>
         </div>
+        </a>
       ))}
     </div>
   );
-};
-
-const styles = {
-  catalog: {
-    width: '80%',
-    margin: '0 auto',
-    padding: '20px',
-    textAlign: 'center',
-  },
-  talkCard: {
-    border: '1px solid #ccc',
-    borderRadius: '8px',
-    padding: '10px',
-    marginBottom: '10px',
-    backgroundColor: '#f9f9f9',
-
-  },
 };
--- src/talks/talks.jsx
+++ src/talks/talks.jsx
@@ -1,16 +1,31 @@
 
 
 import SaturdayMorningPhysicsTalk from './SaturdayMorningPhysicsTalk.jsx'
+import { Navigate } from "react-router-dom";
 
 const talks = [
-    {
-      id: 'saturday-morning-physics',
-      title: 'Saturday Morning Physics',
-      subtitle: 'DESI: Surveying Dark Energy with 5000 Robots',
-      description: 'Public talk',
-      date: '2025-04-12',
-      element: SaturdayMorningPhysicsTalk,
-    },
+  {
+    id: 'saturday-morning-physics',
+    title: 'Saturday Morning Physics',
+    subtitle: 'DESI: Surveying Dark Energy with 5000 Robots',
+    description: 'Public talk at the University of Michigan.',
+    date: '2025-04-12',
+    element: SaturdayMorningPhysicsTalk,
+    background: 'https://s.hdnux.com/photos/01/25/62/04/22487305/4/ratio16x9_1920.jpg',
+  },
+  {
+    id: 'fermilab-2024',
+    title: 'Dark Energy Spectroscopic Instrument',
+    subtitle: 'Cosmology results from Data Release 1',
+    description: 'Seminars given at Perimeter Institute and Fermilab.',
+    date: '2024-11',
+    element: () => {
+      window.location.href = 'https://otavioalves.com/talks/pi2024';
+      return null;
+  },
+    background: 'https://wordpress.wbur.org/wp-content/uploads/2019/03/resized.jpg',
+  },
+  
   ];
 
 export default talks;
